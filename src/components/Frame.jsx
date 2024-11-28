5bf24ae6c52dc1529a0a60cef966987819e914ac
Lucas Padua <lspadua41@gmail.com>
2024-11-28 13:55:26 -0300

    commit second

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,7 +1,62 @@
-import styles from './Frame.module.css'; 
-import { FunnelSimple } from 'phosphor-react'
+import { useState } from "react";
+import styles from "./Frame.module.css";
+import { FunnelSimple } from "phosphor-react";
 
 export function Frame() {
+  const data = [
+    {
+      name: "Pedro",
+      debit: true,
+      date: "25/02/25",
+      value: "R$ 880,00",
+      description: "Recebimento por serviços prestados, desenvolvimento de web site e trafégo pago"
+    },
+    {
+      name: "Vítor",
+      debit: false,
+      date: "09/03/25",
+      value: "R$ 130,00",
+      description: ""
+    },
+    {
+      name: "Lucas",
+      debit: true,
+      date: "17/03/25",
+      value: "R$ 1130,00",
+      description: ""
+    },
+    {
+      name: "Agiota",
+      debit: true,
+      date: "25/02/25",
+      value: "R$ 9000,00",
+      description: ""
+    },
+    {
+      name: "Tigrinho",
+      debit: false,
+      date: "09/03/25",
+      value: "R$ 15000,00",
+      description: ""
+    },
+    {
+      name: "Adega do Zé",
+      debit: true,
+      date: "17/03/25",
+      value: "R$ 300,00",
+      
+    },
+    {
+      name: "Cp2",
+      debit: true,
+      date: "25/03/25",
+      value: "R$ 280,00",
+      
+    }
+  ];
+
+  const [selectedItem, setSelectedItem] = useState(null);
+
   return (
     <div className={styles.cont}>
       <div className={styles.filter}>
@@ -10,7 +65,7 @@ export function Frame() {
           <button className={styles.filterButton}>Ano</button>
         </div>
         <button className={styles.filterIconButton}>
-          <i className="fas fa-filter"></i><FunnelSimple size={20} /> Filtrar
+          <FunnelSimple size={20} /> Filtrar
         </button>
       </div>
 
@@ -19,54 +74,51 @@ export function Frame() {
           <h3>Entradas e Saídas</h3>
           <table className={styles.table}>
             <tbody>
-              <tr>
-                <td>Pedro Paulo</td>
-                <td>Entrada</td>
-                <td>23/02/2025</td>
-                <td>R$ 153,00</td>
-              </tr>
-              <tr>
-                <td>Netflix</td>
-                <td>Saída</td>
-                <td>13/01/2025</td>
-                <td>R$ 99,00</td>
-              </tr>
-              <tr>
-                <td>Carlinhos Maia</td>
-                <td>Entrada</td>
-                <td>23/02/2027</td>
-                <td>R$ 9999,00</td>
-              </tr>
-              <tr>
-                <td>José Pedro</td>
-                <td>Entrada</td>
-                <td>23/02/2024</td>
-                <td>R$ 2,00</td>
-              </tr>
-              <tr>
-                <td>Agiota</td>
-                <td>Saída</td>
-                <td>23/02/2025</td>
-                <td>R$ 1533,00</td>
-              </tr>
-              <tr>
-                <td>Tigrinho</td>
-                <td>Entrada</td>
-                <td>23/02/2025</td>
-                <td>R$ 1599,00</td>
-              </tr>
-              <tr>
-                <td>Neide</td>
-                <td>Saída</td>
-                <td>23/02/2025</td>
-                <td>R$ 7,00</td>
-              </tr>             
+              {data.map((item, index) => (
+                <tr
+                  key={index}
+                  onClick={() => setSelectedItem(item)} 
+                  className={
+                    selectedItem?.name === item.name ? styles.activeRow : ""
+                  }
+                >
+                  <th>{item.name}</th>
+                  <th>{item.debit ? "Entrada" : "Saída"}</th>
+                  <th>{item.date}</th>
+                  <th>{item.value}</th>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
+
         <div className={styles.details}>
-          <p1>Hmm parece que nenhum item foi selecionado ainda</p1>
-          <p2>Selecione algo para que possamos te ajudar!</p2>
+          {selectedItem ? (
+            <div>
+              <h3>Detalhes</h3>
+              <p4>
+                <strong></strong> {selectedItem.date}
+              </p4>
+              <p>
+                <strong>Nome:</strong> {selectedItem.name}
+              </p>
+              <p>
+                <strong>Tipo:</strong> {selectedItem.debit ? "Entrada" : "Saída"}
+              </p>
+              <h4>Descrição</h4>
+              <p7>
+                <strong></strong> {selectedItem.description}
+              </p7>
+              <p8>
+                <strong>Valor</strong> {selectedItem.value}
+              </p8>
+            </div>
+          ) : (
+            <div className={`${styles.detailsEmpty}`}>
+              <p1>Hmm parece que nenhum item foi selecionado ainda</p1>
+              <p>Selecione algo para que possamos te ajudar!</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
